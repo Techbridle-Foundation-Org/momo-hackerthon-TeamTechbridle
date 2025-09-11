0596d34d5eaae9930ff1e6c038e8a92a654f9921
Siyanda Nkosi <siyanda.nkosi@techbridlefoundation.org>
2025-09-11 07:31:01 +0200

    https://github.com/Techbridle-Foundation-Org/momo-hackerthon-TeamTechbridle/tree/MoMoStokvel-Frontend

--- StokePay.api/obj/Debug/net9.0/StokePay.api.AssemblyInfo.cs
+++ StokePay.api/obj/Debug/net9.0/StokePay.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StokePay.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26f83724bcf2b6fad7c4c446d50d24a4830f7c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dab26edf0e406d6eb9e3db09177814e2a2b30b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StokePay.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StokePay.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
